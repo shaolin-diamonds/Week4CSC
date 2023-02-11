dcd59fb730c22666e2125e0fc1a8b45b4de37dd2
shaolin-diamonds <crios0718@gmail.com>
2023-02-10 23:55:23 -0600

    Initial commit

---
+++ TestApi/Controllers/MainApi.cs
@@ -0,0 +1,51 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace TestApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class MainApi : ControllerBase
+    {
+        [HttpPost(Name = "GetWeatherForecast")]
+        public ActionResult<List<string>> IntListWork(List<double> lint)
+        {
+            List<string> slist = new List<string>();
+
+            double mean = 0;
+            double variance = 0;
+            double standard_deviation = 0;
+            double sum = 0;
+
+            double temp = 0;
+            int counter = 0;
+            int loop = 0;
+
+            lint.Sort();
+
+            foreach (int i in lint)
+            {
+                loop++;
+
+                for (counter = 0; counter < lint.Count; counter++)
+                {
+                    sum += lint[counter];
+                }
+
+                mean = sum / (lint.Count - 0);
+
+                for (counter = 0; counter < lint.Count; counter++)
+                {
+                    temp += Math.Pow((lint[counter] - mean), 2);
+                }
+
+                variance = temp / (lint.Count - 0);
+
+                standard_deviation = Math.Sqrt(variance);
+
+                slist.Add("Elements: " + loop + " Current Standard Deviation: " + standard_deviation);
+            }
+
+            return slist;
+        }
+    }
+}
